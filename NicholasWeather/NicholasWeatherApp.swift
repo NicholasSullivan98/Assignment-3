88e92bc954c28d28c6e57a4585f5594b12ce0bff
Nicholas Sullivan <sullnich@sheridancollege.ca>
2024-11-13 18:57:01 -0500

    Initial Commit

---
+++ NicholasWeather/NicholasWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  NicholasWeatherApp.swift
+//  NicholasWeather
+//
+//  Created by Nicholas Sullivan on 2024-11-13.
+//
+
+import SwiftUI
+
+@main
+struct NicholasWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
